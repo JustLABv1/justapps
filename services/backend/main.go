07449f3ba50f6719aafd7a39b9c6d02e3d1f0db9
Justin Neubert <neubert_justin@outlook.com>
2026-03-23 13:51:16 +0100

    chore: update version to 1.0.0-beta.1 in backend and frontend package.json

--- services/backend/main.go
+++ services/backend/main.go
@@ -17,7 +17,7 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-const version string = "0.9.5"
+const version string = "1.0.0-beta.1"
 
 var (
 	configFile = kingpin.Flag("config", "Config file").Short('c').Default("/etc/just-apps/config.yaml").String()
