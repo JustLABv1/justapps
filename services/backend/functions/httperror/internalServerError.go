0b49a85db210b4093a1049cd7c8dd494ae8fdb4e
Justin Neubert <neubert_justin@outlook.com>
2026-03-30 10:36:54 +0200

    fix: format code for consistency in InternalServerError function

--- services/backend/functions/httperror/internalServerError.go
+++ services/backend/functions/httperror/internalServerError.go
@@ -3,17 +3,15 @@ package httperror
 import (
 	"net/http"
 
-	"github.com/gin-gonic/gin"
-)
-
-import (
 	"log"
+
+	"github.com/gin-gonic/gin"
 )
 
 func InternalServerError(context *gin.Context, message string, err error) {
 	errorMessage := "Unknown error"
 	if err != nil {
-		 errorMessage = err.Error()
+		errorMessage = err.Error()
 	}
 	// Fehlerursache ins Backend-Log schreiben
 	log.Printf("[ERROR] %s: %s", message, errorMessage)
